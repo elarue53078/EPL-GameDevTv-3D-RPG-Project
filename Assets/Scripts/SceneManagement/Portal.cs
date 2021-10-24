a78949601dee7e77276a9d51b0fe2fc942b86d61
Eric LaRue <safespace53078@gmail.com>
2021-10-24 13:14:21 -0500

    Added Scene Portal and Scene Manager Script

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print("Portal triggered.");
+            if(other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
