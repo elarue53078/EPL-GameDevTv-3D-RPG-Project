7976f2d3eff834d500510ae1dcad35aabe7d0422
Eric LaRue <safespace53078@gmail.com>
2022-07-26 00:04:29 -0500

    Imported and implemented save system

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            // call to saving system
+            GetComponent<SavingSystem>().Save(defaultSaveFile); 
+        }
+
+        private void Load()
+        {
+            // call to saving system
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+    }
+}
